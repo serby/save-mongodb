2d68777edfad42116f1f8920784ea2c9df0289bb
Paul Serby <paul.serby@clock.co.uk>
2012-10-15 15:47:57 +0100

    delint de;

--- test/mongodb-engine.test.js
+++ test/mongodb-engine.test.js
@@ -1,26 +1,22 @@
-var _ = require('lodash')
-  , async = require('async')
-  , idProperty = '_id'
-  , Db = require('mongodb').Db
+var Db = require('mongodb').Db
   , Server = require('mongodb').Server
   , collection
-  ;
 
-var db = new Db('test', new Server('127.0.0.1', 27017, {}));
+var db = new Db('test', new Server('127.0.0.1', 27017, {}))
 
 function getEngine(callback) {
-  collection.remove({}, function(error) {
-    callback(undefined, require('../lib/mongodb-engine')(collection));
-  });
+  collection.remove({}, function() {
+    callback(undefined, require('../lib/mongodb-engine')(collection))
+  })
 }
 
 require('save/test/engine.tests')('_id', getEngine, function(done) {
   db.open(function(error, connection) {
     connection.collection('test', function(error, c) {
-      collection = c;
-      done();
-    });
-  });
+      collection = c
+      done()
+    })
+  })
 }, function() {
-  db.dropDatabase();
-});
+  db.dropDatabase()
+})
