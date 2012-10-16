6b770c8baac8d4a32dc9024baebee4019131c631
Paul Serby <paul.serby@clock.co.uk>
2012-10-16 17:26:54 +0100

    Setting the connection to safe

--- test/mongodb-engine.test.js
+++ test/mongodb-engine.test.js
@@ -2,7 +2,7 @@ var Db = require('mongodb').Db
   , Server = require('mongodb').Server
   , collection
 
-var db = new Db('test', new Server('127.0.0.1', 27017, {}))
+var db = new Db('test', new Server('127.0.0.1', 27017, { safe: true }))
 
 function getEngine(callback) {
   collection.remove({}, function() {
