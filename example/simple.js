6f02e6238935627b9345c8c3fa62fa851f803263
Mantas <Mikalauskas.it@gmail.com>
2019-08-14 10:22:31 +0100

    Upgrade npm modules, replace istanbul with nyc, add prettier (#29)
    * Upgrade modules, replace istanbul with nyc, add prettier
    
    * Prettify and fix eslint errors
    
    * Drop Node 6 support
    
    * Remove istanbul

--- example/simple.js
+++ example/simple.js
@@ -4,21 +4,27 @@ var save = require('save') // npm install save
 var saveMongodb = require('..')
 
 // Create a db object to a local mongodb database called SimpleExample.
-var db = new Db('test', new Server('127.0.0.1', 27017, {}), { fsync: true, w: 1 })
+var db = new Db('test', new Server('127.0.0.1', 27017, {}), {
+  fsync: true,
+  w: 1
+})
 
 // Open your mongodb database.
-db.open(function (error, connection) {
+db.open(function(error, connection) {
   if (error) return console.error(error.message)
 
   // Get a collection. This will create the collection if it doesn't exist.
-  connection.collection('contact', function (error, collection) {
+  connection.collection('contact', function(error, collection) {
     if (error) return console.error(error.message)
 
     // Create a save object and pass in a mongodb engine.
     var contactStore = save('Contact', { engine: saveMongodb(collection) })
 
     // Then we can create a new object.
-    contactStore.create({ name: 'Paul', email: 'paul@serby.net' }, function (error, contact) {
+    contactStore.create({ name: 'Paul', email: 'paul@serby.net' }, function(
+      error,
+      contact
+    ) {
       if (error) return console.error(error.message)
 
       // The created 'contact' is returned and has been given an _id
